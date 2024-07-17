f4b76625e47f6baa8ca189e5c4bee42398cd374c
Angelo Gomez <adgomez.dev@gmail.com>
2024-07-17 16:18:39 -0400

    Finished task 5 and 6

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,10 +4,35 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+// Check if a user with the given username already exists
+const doesExist = (username) => {
+    // Filter the users array for any user with the same username
+    let userswithsamename = users.filter((user) => {
+        return user.username === username;
+    });
+    // Return true if any user with the same username is found, otherwise false
+    if (userswithsamename.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+    const password = req.body.password;
+    if (username && password){
+    // Check if the user does not already exist
+        if (!doesExist(username)) {
+            // Add the new user to the users array
+            users.push({"username": username, "password": password});
+            return res.status(200).json({message: "User successfully registered. Now you can login"});
+        } else {
+            return res.status(404).json({message: "User already exists!"});
+        }
+    }
+    // Return error if username or password is missing
+    return res.status(404).json({message: "Unable to register user."});
 });
 
 // Get the book list available in the shop
@@ -18,8 +43,8 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  let isbn = req.params.isbn;
-  res.status(200).send(books[isbn])
+    let isbn = req.params.isbn;
+    res.status(200).send(books[isbn]);
  });
   
 // Get book details based on author
@@ -56,8 +81,13 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let isbn = req.params.isbn;
+    try{
+        res.status(200).send(books[isbn].reviews);
+    }
+    catch (TypeError){
+        res.status(500).send({message:"A book with that ISBN does not exist"})
+    }
 });
 
 module.exports.general = public_users;
